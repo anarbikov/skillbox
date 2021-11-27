4ea1f2dc7749ebf44a5be80dc598db914f7ffd54
Anvyar <anvyar.narbikov@ya.ru>
2021-11-27 16:38:27 +0300

    Finished task for lesson 02

---
+++ 02_HelloKotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    println("Добрый день!")
+    print("Моё ")
+    print("первое приложение ")
+    /*print("представляло из себя ")
+    print("калькулятор")*/
+    //print("на Kotlin")
+
+
+
+}
