e8c1b00c3bc37e07a1640a668a9e1b113682d7b1
Anvyar <anvyar.narbikov@ya.ru>
2021-12-04 00:00:08 +0300

    Task for chapter 4 finished

--- 04_Functions/src/main/kotlin/Main.kt
+++ 04_Functions/src/main/kotlin/Main.kt
@@ -1,7 +1,25 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val finalString = """F2p)v"y233{0->c}ttelciFc"""
+    val sumString = {a: String,b:String -> a+b}
+    println(sumString(firstHalfOfTheString(finalString),secondHalfOfTheString(finalString)))
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun firstHalfOfTheString(a: String): String {
+    val firstSplit = a.take((a.length / 2))
+    val shiftedStringTwo = shift(firstSplit, 1)
+    val withS = shiftedStringTwo.replace("5", "s")
+    val withU = withS.replace("4", "u")
+    val shiftedStringThree = shift(withU, -3)
+    return shiftedStringThree.replace("0", "o")
+}
+
+fun secondHalfOfTheString(a: String): String {
+    var secondSplit = a.takeLast((a.length / 2))
+    secondSplit = secondSplit.reversed()
+    val shiftedString = shift(secondSplit, -4)
+    return shiftedString.replace("_", " ")
+}
+
+fun shift(a: String, b: Int): String {
+    return a.map { it -> it + b }.joinToString("")
 }
