d68dea5bd268b9d61123d43db9a62ababbb3db67
Anvyar <anvyar.narbikov@ya.ru>
2022-01-06 17:00:03 +0300

    06.01.2022 not finished

---
+++ 13_Coroutines/src/main/kotlin/Fibonacci.kt
@@ -0,0 +1,24 @@
+import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.yield
+import java.math.BigInteger
+
+object Fibonacci {
+    suspend fun take(sequenceNumber:Int):BigInteger {
+        yield()
+        var b = 1.toBigInteger()
+        var c = 1.toBigInteger()
+        var sum = 0.toBigInteger()
+        when (sequenceNumber) {
+            1 -> sum = 0.toBigInteger()
+            in 2..3 -> sum = 1.toBigInteger()
+            in 4..sequenceNumber -> {
+                for (i in 4..sequenceNumber) {
+                    sum = b + c
+                    b = c
+                    c = sum
+                }
+            }
+        }
+        return sum
+    }
+}
--- 13_Coroutines/src/main/kotlin/Main.kt
+++ 13_Coroutines/src/main/kotlin/Main.kt
@@ -1,3 +1,36 @@
-fun main() {
+import kotlinx.coroutines.*
 
+suspend fun main() {
+    runBlocking {
+        launch { println("${Fibonacci.take(5)} - Fibo(5)") }
+        launch { println("${Fibonacci.take(10)} - Fibo(10)") }
+        launch { println("${Fibonacci.take(15)} - Fibo(15)") }
+        launch { println("${Fibonacci.take(25)} - Fibo(25)") }
+        launch { println("${Fibonacci.take(20)} - Fibo(20)") }
+        launch {
+            for (i in 4..20) {
+                if (i == 8) this.cancel()
+                else {
+                    println("${Fibonacci.take(i)} - Fibo($i)")
+                }
+            }
+        }
+        launch {
+//        delay(300)
+            launch { println("${Fibonacci.take(300)} - Fibo(300)") }
+        }
+
+
+
+        withTimeout(1000) {
+                launch {
+                    try {
+                    println(Fibonacci.take(999999))
+                } catch (e:TimeoutCancellationException) {println("Fibo(999999) is cancelled - Timed out")}
+            }
+        }
+        launch { println("${Fibonacci.take(120)} - Fibo(120)") }
+
+
+    }
 }
